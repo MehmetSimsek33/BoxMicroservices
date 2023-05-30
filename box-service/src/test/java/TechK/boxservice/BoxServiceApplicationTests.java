06c79bc82bee3522594af3274d992287a6573a63
Mehmet <mehmetsimsek3212368@hotmail.com>
2023-05-30 09:28:50 +0300

    first commit

---
+++ box-service/src/test/java/TechK/boxservice/BoxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package TechK.boxservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ common/src/main/java/TechK/common/utilities/results/SuccesResult.java
@@ -0,0 +1,16 @@
+package TechK.common.utilities.results;
+
+
+
+public class SuccesResult extends Result {
+
+	public SuccesResult() {
+		super(true);
+	}
+
+	public SuccesResult(String message) {
+		super(true, message);
+
+	}
+
+}
